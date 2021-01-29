ecb853320ce14dfeb45010a0deddb6f998939574
Matt Hurst <matt@workpail.com>
2021-01-29 13:05:20 -0800

    Sort members and export wrapper
    - Resolves #1
    - Only export types from wrapper

--- lib/sign_in_with_apple_js.dart
+++ lib/sign_in_with_apple_js.dart
@@ -5,15 +5,16 @@ import 'dart:html' as html;
 
 import 'apple_id_auth_js.dart' as apple_id_auth;
 
+export 'apple_id_auth_js.dart' hide signIn, init;
+
 StreamSubscription _subAppleIDSignInOnSuccess, _subAppleIDSignInOnFailure;
 
-/// Listen to the success event from Sign in with Apple JS
-StreamSubscription<html.Event> listenAppleIDSignInOnSuccess(
-    Function(html.Event) callback) {
-  _subAppleIDSignInOnSuccess?.cancel();
-  return _subAppleIDSignInOnSuccess =
-      html.document.on['AppleIDSignInOnSuccess'].listen(callback);
-}
+/// Initialize Sign in with Apple JS using a configuration object
+///
+/// Calling [initSignInWithApple] is optional if these options are
+/// specified in your html.
+void initSignInWithApple(apple_id_auth.ClientConfigI options) =>
+    apple_id_auth.init(options);
 
 /// Listen to the failure event from Sign in with Apple JS
 StreamSubscription<html.Event> listenAppleIDSignInOnFailure(
@@ -23,12 +24,13 @@ StreamSubscription<html.Event> listenAppleIDSignInOnFailure(
       html.document.on['AppleIDSignInOnFailure'].listen(callback);
 }
 
-/// Initialize Sign in with Apple JS using a configuration object
-///
-/// Calling [initSignInWithApple] is optional if these options are
-/// specified in your html.
-void initSignInWithApple(apple_id_auth.ClientConfigI options) =>
-    apple_id_auth.init(options);
+/// Listen to the success event from Sign in with Apple JS
+StreamSubscription<html.Event> listenAppleIDSignInOnSuccess(
+    Function(html.Event) callback) {
+  _subAppleIDSignInOnSuccess?.cancel();
+  return _subAppleIDSignInOnSuccess =
+      html.document.on['AppleIDSignInOnSuccess'].listen(callback);
+}
 
 /// Make asynchronous call to Sign in with Apple
 ///
